cdde225ff39dd36de8e8f60feb58fa0d2acf3687
Akshay-Apex <127332144+Akshay-Apex@users.noreply.github.com>
2024-11-17 19:28:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const query = document.getElementById("query");
 const search_button = document.getElementById("search-button");
 let search_result = document.getElementById("search-result");
+let sfw = 1;
 
 
 query.addEventListener("keydown", (event) => {
@@ -10,13 +11,48 @@ query.addEventListener("keydown", (event) => {
 });
 
 
+function sfw_Switch() {  
+  const sfw_button = document.getElementById("sfw-button");
+  if(sfw == 1) {
+    sfw = 0;
+    sfw_button.innerText = "NSFW";
+    sfw_button.style.backgroundColor = "#fa4679";
+    sfw_button.style.color = "white";
+    sfw_button.classList.remove("sfw");
+    sfw_button.classList.add("nsfw");
+  } else {
+    sfw = 1;
+    sfw_button.innerText = "SFW";
+    sfw_button.style.backgroundColor = "#75f7d4";
+    sfw_button.style.color = "#115f58";
+    sfw_button.classList.remove("nsfw");
+    sfw_button.classList.add("sfw");   
+  }
+}
+
+function toggleButton() {
+  const sfw_button = document.getElementById("sfw-button");
+  sfw = (sfw == 1) ? 0 : 1;
+  
+  if (sfw_button.innerText === "SFW") {
+    sfw_button.innerText = "NSFW";
+    sfw_button.classList.remove("sfw");
+    sfw_button.classList.add("nsfw");
+  } else {
+    sfw_button.innerText = "SFW";
+    sfw_button.classList.remove("nsfw");
+    sfw_button.classList.add("sfw");
+  }
+}
+
+
 async function getAnimeList() {  
   search_result.innerHTML = `<span id="loading">Loading...</span> 
                              <span id="animeListCount"></span>`;
   const animeListCount = document.getElementById("animeListCount");
   const loading = document.getElementById("loading");
 
-  const url = `https://api.jikan.moe/v4/anime?q=${encodeURIComponent(query.value.trim())}&order_by=end_date&sort=desc`;  
+  const url = `https://api.jikan.moe/v4/anime?q=${encodeURIComponent(query.value.trim())}&sfw=${sfw}`;  
   try {
     loading.style.display = "block";
     const response = await fetch(url);
@@ -28,7 +64,7 @@ async function getAnimeList() {
       animeListCount.innerText = `Showing ${dataLength} results: `;
       for(let i = 0; i < dataLength; i++) {
         const animeData = data.data[i];      
-        const genresList = animeData.genres.map(genre => genre.name).join(', ');
+        const genresList = animeData.genres.map(genre => genre.name).join(' / ');
 
         search_result.innerHTML += `
           <div class="card-wrapper">
