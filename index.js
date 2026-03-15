c78eb0a3be1d18d8835c500e3a8e784abfb55f30
Akshay-Apex <127332144+Akshay-Apex@users.noreply.github.com>
2026-03-15 11:31:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -391,7 +391,7 @@ function takeScreenshot(wrapper, imgContainer, img) {
       allowTaint: true,
       useCORS: true,       
       windowWidth: '440px',
-      x: -0.15,              
+      x: -0.1,              
       y: 0,
       scale: 5
   }).then(canvas => {        
