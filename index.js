991eec513fc24820cb16a2a7019c79e56346545b
Akshay-Apex <127332144+Akshay-Apex@users.noreply.github.com>
2026-03-15 11:26:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -391,8 +391,8 @@ function takeScreenshot(wrapper, imgContainer, img) {
       allowTaint: true,
       useCORS: true,       
       windowWidth: '440px',
-      x: 0.15,              
-      y: -0.2,
+      x: -0.5,              
+      y: 0,
       scale: 5
   }).then(canvas => {        
       const downloadLink = document.createElement('a');
